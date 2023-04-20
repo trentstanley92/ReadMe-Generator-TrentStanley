e1e55fb136416f5c18f5c0f26617c5016e52835e
trentstanley92 <trentstanley92@gmail.com>
2023-04-20 13:07:05 -0400

    updated index.js with init function

--- Develop/index.js
+++ Develop/index.js
@@ -1,76 +1,63 @@
-// // TODO: Include packages needed for this application
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
-
-
-
+// packages for using application
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// Prompt user for input
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'projectTitle',
-      message: 'Enter the title of your project:',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Enter a description for your project:',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Enter installation instructions for your project:',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Enter usage information for your project:',
-    },
-    {
-      type: 'input',
-      name: 'contribution',
-      message: 'Enter contribution guidelines for your project:',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Enter test instructions for your project:',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
-    },
-    {
-      type: 'input',
-      name: 'githubUsername',
-      message: 'Enter your GitHub username:',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address:',
-    },
-  ])
-  .then((answers) => {
-    // Generate README content
-    const readmeContent = `# ${answers.projectTitle}
+// function to initialize app
+function init() {
+
+  // Prompt user for input using inquirer to get read me details
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Enter the title of your project:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions for your project:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information for your project:',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines for your project:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions for your project:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+    ])
+    .then((answers) => {
+      // Generate README content using user inputted answers and project title
+      const readmeContent = `# ${answers.projectTitle}
 
 ## Description
 ${answers.description}
@@ -104,12 +91,17 @@ For questions or inquiries, please contact me via GitHub or email:
 - Email: ${answers.email}
 `;
 
-    // Write README file
-    fs.writeFile('README.md', readmeContent, (err) => {
-      if (err) throw err;
-      console.log('README.md file has been generated successfully!');
+      // Write README file
+      fs.writeFile('README.md', readmeContent, (err) => {
+        if (err) throw err;
+        console.log('README.md file has been generated successfully!');
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+
+}
+
+// Call the init() function to initialize the app
+init();
